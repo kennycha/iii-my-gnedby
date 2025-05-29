798ee6fcfc25760ad0c261cc34e0ecff5596f43b
kennycha <dudqn136@naver.com>
2025-05-29 20:31:34 +0900

    ci/cd: change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/iii-my-gnedby/',
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     exclude: ['onnxruntime-web']
