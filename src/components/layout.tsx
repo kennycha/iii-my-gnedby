2bb594491293a5a1aa9da86fcb6c88c7e32f7239
kennycha <dudqn136@naver.com>
2025-05-31 09:18:25 +0900

    feat: change content height to dvh

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from "react";
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <div className="bg-[#122118]">
-      <div className="max-w-md mx-auto h-screen bg-[#1b3124]">{children}</div>
+      <div className="max-w-md mx-auto h-dvh bg-[#1b3124]">{children}</div>
     </div>
   );
 }
